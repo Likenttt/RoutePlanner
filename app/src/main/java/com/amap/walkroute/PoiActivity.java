b19846aa85f48b2bc0ac5e0d606f57a751239fc1
likenttt <1411390610@qq.com>
2018-10-20 23:28:00 +0800

    update poiactivity at Sat Oct 20 23:28:00 CST 2018

--- app/src/main/java/com/amap/walkroute/PoiActivity.java
+++ app/src/main/java/com/amap/walkroute/PoiActivity.java
@@ -5,9 +5,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.amap.api.location.AMapLocationClient;
+import com.amap.api.location.AMapLocationClientOption;
 import com.amap.api.maps.AMap;
 import com.amap.api.maps.CameraUpdateFactory;
 import com.amap.api.maps.SupportMapFragment;
@@ -41,6 +44,10 @@ public class PoiActivity extends FragmentActivity implements
     private TextView mKeywordsTextView;
     private Marker mPoiMarker;
     private ImageView mCleanKeyWords;
+    private ImageButton mLocateMine;
+
+    private AMapLocationClient locationClient = null;
+    private AMapLocationClientOption locationOption = null;
 
     public static final int REQUEST_CODE = 100;
     public static final int RESULT_CODE_INPUTTIPS = 101;
@@ -50,11 +57,21 @@ public class PoiActivity extends FragmentActivity implements
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_poi);
+        mLocateMine = (ImageButton) findViewById(R.id.location_bt);
+        mLocateMine.setOnClickListener(this);
+
 
         mCleanKeyWords = (ImageView) findViewById(R.id.clean_keywords);
         mCleanKeyWords.setOnClickListener(this);
         init();
         mKeyWords = "";
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        destroyLocation();
+
     }
 
     /**
@@ -234,7 +251,7 @@ public class PoiActivity extends FragmentActivity implements
                 doSearchQuery(keywords);
             }
             mKeywordsTextView.setText(keywords);
-            if (!keywords.equals("")) {
+            if (keywords != null && !keywords.equals("")) {
                 mCleanKeyWords.setVisibility(View.VISIBLE);
             }
         }
@@ -274,11 +291,136 @@ public class PoiActivity extends FragmentActivity implements
                 mKeywordsTextView.setText("");
                 mAMap.clear();
                 mCleanKeyWords.setVisibility(View.GONE);
+                break;
             case R.id.location_bt:
                 //todo: 定位
-
+                ToastUtil.show(PoiActivity.this, "点击定位按钮");
+                initLocation();
+//                locationClient.
+                break;
             default:
                 break;
+        }
+    }
+
+    /**
+     * 初始化定位
+     *
+     * @author hongming.wang
+     * @since 2.8.0
+     */
+    private void initLocation() {
+        //初始化client
+        locationClient = new AMapLocationClient(this.getApplicationContext());
+        locationOption = getDefaultOption();
+        //设置定位参数
+        locationClient.setLocationOption(locationOption);
+        // 设置定位监听
+        //locationClient.setLocationListener(locationListener);
+    }
+
+    /**
+     * 默认的定位参数
+     *
+     * @author hongming.wang
+     * @since 2.8.0
+     */
+    private AMapLocationClientOption getDefaultOption() {
+        AMapLocationClientOption mOption = new AMapLocationClientOption();
+        mOption.setLocationMode(AMapLocationClientOption.AMapLocationMode.Hight_Accuracy);//可选，设置定位模式，可选的模式有高精度、仅设备、仅网络。默认为高精度模式
+        mOption.setGpsFirst(false);//可选，设置是否gps优先，只在高精度模式下有效。默认关闭
+        mOption.setHttpTimeOut(30000);//可选，设置网络请求超时时间。默认为30秒。在仅设备模式下无效
+        mOption.setInterval(2000);//可选，设置定位间隔。默认为2秒
+        mOption.setNeedAddress(true);//可选，设置是否返回逆地理地址信息。默认是true
+        mOption.setOnceLocation(false);//可选，设置是否单次定位。默认是false
+        mOption.setOnceLocationLatest(false);//可选，设置是否等待wifi刷新，默认为false.如果设置为true,会自动变为单次定位，持续定位时不要使用
+        AMapLocationClientOption.setLocationProtocol(AMapLocationClientOption.AMapLocationProtocol.HTTP);//可选， 设置网络请求的协议。可选HTTP或者HTTPS。默认为HTTP
+        mOption.setSensorEnable(false);//可选，设置是否使用传感器。默认是false
+        mOption.setWifiScan(true); //可选，设置是否开启wifi扫描。默认为true，如果设置为false会同时停止主动刷新，停止以后完全依赖于系统刷新，定位位置可能存在误差
+        mOption.setLocationCacheEnable(true); //可选，设置是否使用缓存定位，默认为true
+        mOption.setGeoLanguage(AMapLocationClientOption.GeoLanguage.DEFAULT);//可选，设置逆地理信息的语言，默认值为默认语言（根据所在地区选择语言）
+        return mOption;
+    }
+
+    /**
+     * 定位监听
+     */
+//    AMapLocationListener locationListener = new AMapLocationListener() {
+//        @Override
+//        public void onLocationChanged(AMapLocation location) {
+//            if (null != location) {
+//
+//                StringBuffer sb = new StringBuffer();
+//                //errCode等于0代表定位成功，其他的为定位失败，具体的可以参照官网定位错误码说明
+//                if(location.getErrorCode() == 0){
+//                    sb.append("定位成功" + "\n");
+//                    sb.append("定位类型: " + location.getLocationType() + "\n");
+//                    sb.append("经    度    : " + location.getLongitude() + "\n");
+//                    sb.append("纬    度    : " + location.getLatitude() + "\n");
+//                    sb.append("精    度    : " + location.getAccuracy() + "米" + "\n");
+//                    sb.append("提供者    : " + location.getProvider() + "\n");
+//
+//                    sb.append("速    度    : " + location.getSpeed() + "米/秒" + "\n");
+//                    sb.append("角    度    : " + location.getBearing() + "\n");
+//                    // 获取当前提供定位服务的卫星个数
+//                    sb.append("星    数    : " + location.getSatellites() + "\n");
+//                    sb.append("国    家    : " + location.getCountry() + "\n");
+//                    sb.append("省            : " + location.getProvince() + "\n");
+//                    sb.append("市            : " + location.getCity() + "\n");
+//                    sb.append("城市编码 : " + location.getCityCode() + "\n");
+//                    sb.append("区            : " + location.getDistrict() + "\n");
+//                    sb.append("区域 码   : " + location.getAdCode() + "\n");
+//                    sb.append("地    址    : " + location.getAddress() + "\n");
+//                    sb.append("兴趣点    : " + location.getPoiName() + "\n");
+//                } else {
+//                    sb.append("定位失败" + "\n");
+//                    sb.append("错误码:" + location.getErrorCode() + "\n");
+//                    sb.append("错误信息:" + location.getErrorInfo() + "\n");
+//                    sb.append("错误描述:" + location.getLocationDetail() + "\n");
+//                }
+//                sb.append("***定位质量报告***").append("\n");
+//                sb.append("* WIFI开关：").append(location.getLocationQualityReport().isWifiAble() ? "开启":"关闭").append("\n");
+//                sb.append("* GPS星数：").append(location.getLocationQualityReport().getGPSSatellites()).append("\n");
+//                sb.append("* 网络类型：" + location.getLocationQualityReport().getNetworkType()).append("\n");
+//                sb.append("* 网络耗时：" + location.getLocationQualityReport().getNetUseTime()).append("\n");
+//                sb.append("****************").append("\n");
+//
+//                String result = sb.toString();
+//                tvResult.setText(result);
+//            } else {
+//                tvResult.setText("定位失败，loc is null");
+//            }
+//        }
+//    };
+
+    /**
+     * 停止定位
+     *
+     * @since 2.8.0
+     * @author hongming.wang
+     *
+     */
+    private void stopLocation(){
+        // 停止定位
+        locationClient.stopLocation();
+    }
+
+    /**
+     * 销毁定位
+     *
+     * @since 2.8.0
+     * @author hongming.wang
+     *
+     */
+    private void destroyLocation(){
+        if (null != locationClient) {
+            /**
+             * 如果AMapLocationClient是在当前Activity实例化的，
+             * 在Activity的onDestroy中一定要执行AMapLocationClient的onDestroy
+             */
+            locationClient.onDestroy();
+            locationClient = null;
+            locationOption = null;
         }
     }
 
